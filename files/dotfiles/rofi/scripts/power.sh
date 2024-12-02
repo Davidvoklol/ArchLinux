bc3d0d90a071f779f2b30e183cccc8fc318ad054
David <polyak.daavid@gmail.com>
2024-12-02 23:15:14 +0100

    commit

---
+++ files/dotfiles/rofi/scripts/power.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+declare -A options
+options["Shutdown"]="echo shutting down..."
+options["Reboot"]="echo rebooting..."
+options["Logout"]="echo logging out..."
+options["Lock"]="echo locking screen..."
